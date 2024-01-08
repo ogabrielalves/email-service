117ab27eb7506caccdcec4e281ae1ff7a38a33b7
Gabriel Alves <gabrielalvessilva02@outlook.com>
2024-01-08 01:55:50 -0300

    [CHORE] - Correct parameter with wrong name

--- src/main/java/com/ogabrielalves/emailservice/adapters/EmailSenderGateway.java
+++ src/main/java/com/ogabrielalves/emailservice/adapters/EmailSenderGateway.java
@@ -1,5 +1,5 @@
 package com.ogabrielalves.emailservice.adapters;
 
 public interface EmailSenderGateway {
-    void sendEmail(String to, String subjetct, String body);
+    void sendEmail(String to, String subject, String body);
 }

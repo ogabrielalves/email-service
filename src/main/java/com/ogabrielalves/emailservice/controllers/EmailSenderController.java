06c1b426c605908ce56df987424554b0df494e8b
Gabriel Alves <gabrielalvessilva02@outlook.com>
2024-01-08 02:10:23 -0300

    [FEAT] - Create controller

---
+++ src/main/java/com/ogabrielalves/emailservice/controllers/EmailSenderController.java
@@ -0,0 +1,34 @@
+package com.ogabrielalves.emailservice.controllers;
+
+import com.ogabrielalves.emailservice.application.EmailSenderService;
+import com.ogabrielalves.emailservice.core.EmailRequest;
+import com.ogabrielalves.emailservice.core.exceptions.EmailServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
+public class EmailSenderController {
+
+    private final EmailSenderService emailSenderService;
+
+    @Autowired
+    public EmailSenderController(EmailSenderService emailSenderService) {
+        this.emailSenderService = emailSenderService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request) {
+        try {
+            this.emailSenderService.sendEmail(request.to(), request.subject(), request.body());
+            return ResponseEntity.ok("Email send successfully!");
+        } catch (EmailServiceException exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while sending email.");
+        }
+    }
+}

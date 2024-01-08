b6c202b16de6a609215e059d188defce885483d3
Gabriel Alves <gabrielalvessilva02@outlook.com>
2024-01-08 02:10:38 -0300

    [FEAT] - Create AWS SES

---
+++ src/main/java/com/ogabrielalves/emailservice/infra/ses/SesEmailSender.java
@@ -0,0 +1,38 @@
+package com.ogabrielalves.emailservice.infra.ses;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.*;
+import com.ogabrielalves.emailservice.adapters.EmailSenderGateway;
+import com.ogabrielalves.emailservice.core.exceptions.EmailServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SesEmailSender implements EmailSenderGateway {
+
+    private final AmazonSimpleEmailService amazonSimpleEmailService;
+
+    @Autowired
+    public SesEmailSender(AmazonSimpleEmailService amazonSimpleEmailService) {
+        this.amazonSimpleEmailService = amazonSimpleEmailService;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        SendEmailRequest request = new SendEmailRequest()
+                .withSource("gabrielalvessilva02@outlook.com")
+                .withDestination(new Destination().withToAddresses(to))
+                .withMessage(new Message()
+                        .withSubject(new Content(subject))
+                        .withBody(new Body().withText(new Content(body)))
+                );
+
+        try {
+            this.amazonSimpleEmailService.sendEmail(request);
+        } catch (AmazonServiceException exception) {
+            throw new EmailServiceException("An error occurred while attempting to send the email.", exception);
+        }
+
+    }
+}

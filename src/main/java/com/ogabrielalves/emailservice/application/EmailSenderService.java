3e8c097b9dab73cb84e1289f8e987f09b2210834
Gabriel Alves <gabrielalvessilva02@outlook.com>
2024-01-08 01:56:21 -0300

    [ADD] - Add Service Annotation

--- src/main/java/com/ogabrielalves/emailservice/application/EmailSenderService.java
+++ src/main/java/com/ogabrielalves/emailservice/application/EmailSenderService.java
@@ -3,7 +3,9 @@ package com.ogabrielalves.emailservice.application;
 import com.ogabrielalves.emailservice.adapters.EmailSenderGateway;
 import com.ogabrielalves.emailservice.core.EmailSenderUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailSenderService implements EmailSenderUseCase {
 
     private final EmailSenderGateway emailSenderGateway;
